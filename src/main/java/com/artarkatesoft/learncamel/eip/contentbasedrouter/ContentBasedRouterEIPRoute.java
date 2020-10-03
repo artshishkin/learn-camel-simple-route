e6a6bccc739afdcb0321e7b4509620ab755eecff
Art <d.art.shishkin@gmail.com>
2020-10-03 12:59:42 +0300

    42 Content Based Router - part 1

---
+++ src/main/java/com/artarkatesoft/learncamel/eip/contentbasedrouter/ContentBasedRouterEIPRoute.java
@@ -0,0 +1,17 @@
+package com.artarkatesoft.learncamel.eip.contentbasedrouter;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class ContentBasedRouterEIPRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("file:data/eip/content_based_router/input?noop=true")
+                .to("log:?level=INFO&showBody=true&showHeaders=true")
+                .choice()
+                .when(header("CamelFileName").endsWith(".txt")).to("file:data/eip/content_based_router/output/txt")
+                .when(header("CamelFileName").endsWith(".json")).to("file:data/eip/content_based_router/output/json")
+                .when(header("CamelFileName").endsWith(".html")).to("file:data/eip/content_based_router/output/html")
+                .otherwise().to("file:data/eip/content_based_router/output/other")
+        ;
+    }
+}
